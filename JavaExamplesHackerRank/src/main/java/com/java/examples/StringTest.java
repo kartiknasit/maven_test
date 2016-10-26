3ac9551a9b1fc919168951bf3bdc8eac7568e0cf
Kartik Patel <kartiknasit@gmail.com>
2016-10-26 14:15:01 +0530

    Testing Maven Project.

---
+++ JavaExamplesHackerRank/src/main/java/com/java/examples/StringTest.java
@@ -0,0 +1,11 @@
+package com.java.examples;
+
+/**
+ * Created by kartik.patel on 10/26/2016.
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
